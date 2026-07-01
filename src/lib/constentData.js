bbf27bf13de5c1487e2ba6b01771fa96b221760b
Lalit Kumar <kumar932687@gmail.com>
2026-07-01 10:24:58 +0530

    intial commit

---
+++ src/lib/constentData.js
@@ -0,0 +1,38 @@
+import certificateImg from "@/app/api/helperfile/Images/CertificateImg";
+import logo from "@/app/api/helperfile/Images/logo";
+import semkariLogo from "@/app/api/helperfile/Images/semkariLogo";
+
+export const TrsutData={
+    name:"श्री राधे कृिष्ना सेवा फाउन्डेशन",
+    cityState:"सुरत-गुजरात",
+    address:"हेड ओफीसः ३०१,वृंदावन अपार्टमेन्ट,थर्ड फलोर,पारस पोलीस चोकी के पास,कतारगाम,सुरत",
+    contact:"+91 9408486065",
+    contactPerson:"मोदी देवेन्द्रकुमार छगनलाल (रामसन)",
+    trustPresident:"मोदी संजयकुमार रणछोड़भाई (खीमत),मोदी भावेशकुमार गणेशभाई (धानेरा)",
+    frameImg:certificateImg,
+    email:"",
+    website:"",
+    regNo:null,
+    logo:logo,
+    RightLogo:null,
+    headerImg:"/Images/headerImg.png",
+    topTitle:[
+        "॥ ॐ जय श्री गणेशाय नमः ॥",
+        "।। श्री राधेकृष्नाय नमः ।।"
+    ]
+}
+
+export const pdfColors={
+bgColor:'#ffffff',
+borderColor:'#0d5497',
+headingColor:'#eb1e23',
+schemeColor:'#1a0f5e',
+infoLabelColor:'#EA1F25',
+infoValueColor:'#1a0f5e',
+filterLabelColor:'#8B0000',
+filterValueColor:'#1a0f5e',
+summaryColor:'#1a0f5e',
+totalCountColor:'#d4af37',
+programHeaderColor:'#1a0f5e',
+programHeaderBorderColor:'#d4af37',
+}
